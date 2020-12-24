6504b153f6195ba3f53555402f1487308a8c03b9
kleinfreund <philipprudloff@fastmail.com>
2020-12-24 11:34:10 +0100

    Add overlay mode to gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,46 +1,132 @@
 (function () {
   document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.remove("js-disabled");
-    initGallery();
+
+    for (const gallery of document.querySelectorAll('.gallery')) {
+      initGallery(gallery)
+    }
   });
 })();
 
-function initGallery() {
-  const galleries = document.querySelectorAll('.gallery')
+/**
+ * @param {Element} gallery
+ */
+function initGallery(gallery) {
+  const scrollContainer = gallery.querySelector('.gallery-scroll-container')
+  const toggleOverlayButtons = gallery.querySelectorAll('.gallery-toggle-overlay-button')
+  const closeButton = gallery.querySelector('.gallery-close-overlay-button')
+  const prevButton = gallery.querySelector('.gallery-controls__prev-button')
+  const nextButton = gallery.querySelector('.gallery-controls__next-button')
 
-  for (const gallery of galleries) {
-    const scrollContainer = gallery.querySelector('.gallery-scroll-container')
-    const prevButton = gallery.querySelector('.gallery-controls__prev-button')
-    const nextButton = gallery.querySelector('.gallery-controls__next-button')
+  if (scrollContainer instanceof HTMLElement) {
+    for (const toggleOverlayButton of toggleOverlayButtons) {
+      toggleOverlayButton.addEventListener('click', function () {
+        toggleOverlay(gallery, scrollContainer, prevButton, nextButton)
+      })
+    }
 
-    if (scrollContainer instanceof HTMLElement && prevButton instanceof HTMLButtonElement && nextButton instanceof HTMLButtonElement) {
+    closeButton.addEventListener('click', function (event) {
+      event.stopPropagation()
+      toggleOverlay(gallery, scrollContainer, prevButton, nextButton)
+    })
+
+    if (prevButton instanceof HTMLButtonElement && nextButton instanceof HTMLButtonElement) {
       prevButton.disabled = getGalleryItemIndex(scrollContainer) === 0
 
       prevButton.addEventListener('click', function () {
-        nextButton.disabled = false
-        const galleryItemIndex = getGalleryItemIndex(scrollContainer) - 1
-        scrollContainer.scrollLeft = galleryItemIndex * scrollContainer.clientWidth
-        prevButton.disabled = galleryItemIndex <= 0
+        goToPreviousImage(scrollContainer, prevButton, nextButton)
       })
 
       nextButton.addEventListener('click', function () {
-        prevButton.disabled = false
-        const galleryItemIndex = getGalleryItemIndex(scrollContainer) + 1
-        scrollContainer.scrollLeft = galleryItemIndex * scrollContainer.clientWidth
-        nextButton.disabled = galleryItemIndex >= scrollContainer.children.length - 1
+        goToNextImage(scrollContainer, prevButton, nextButton)
       })
 
       scrollContainer.addEventListener('scroll', debounce(function () {
-        const galleryItemIndex = getGalleryItemIndex(scrollContainer)
-        prevButton.disabled = galleryItemIndex <= 0
-        nextButton.disabled = galleryItemIndex >= scrollContainer.children.length - 1
+        updateGalleryButtons(scrollContainer, prevButton, nextButton)
       }, 50))
     }
   }
 }
 
 /**
- * @param {Element} scrollContainer
+ * @param {Element} gallery
+ * @param {HTMLElement} scrollContainer
+ * @param {Element | null} prevButton
+ * @param {Element | null} nextButton
+ */
+function toggleOverlay(gallery, scrollContainer, prevButton, nextButton) {
+  const galleryItemIndex = getGalleryItemIndex(scrollContainer)
+
+  if (gallery.classList.contains('gallery--is-overlay')) {
+    gallery.classList.remove('gallery--is-overlay')
+    document.body.style.removeProperty('overflow')
+  } else {
+    gallery.classList.add('gallery--is-overlay')
+    document.body.style.setProperty('overflow', 'hidden')
+
+    if (!gallery.hasAttribute('data-has-loaded-high-res-images')) {
+      gallery.setAttribute('data-has-loaded-high-res-images', '')
+      const images = gallery.querySelectorAll('.gallery-item__image')
+      for (const image of images) {
+        const highResSource = image.getAttribute('data-high-res-src')
+        if (highResSource) {
+          image.setAttribute('src', highResSource)
+          image.removeAttribute('data-high-res-src')
+        }
+      }
+    }
+  }
+
+  if (prevButton instanceof HTMLButtonElement && nextButton instanceof HTMLButtonElement) {
+    setTimeout(() => {
+      goToImage(scrollContainer, prevButton, nextButton, galleryItemIndex)
+    }, 0)
+  }
+}
+
+/**
+ * @param {HTMLElement} scrollContainer
+ * @param {HTMLButtonElement} prevButton
+ * @param {HTMLButtonElement} nextButton
+ */
+function goToPreviousImage(scrollContainer, prevButton, nextButton) {
+  goToImage(scrollContainer, prevButton, nextButton, getGalleryItemIndex(scrollContainer) - 1)
+}
+
+/**
+ * @param {HTMLElement} scrollContainer
+ * @param {HTMLButtonElement} prevButton
+ * @param {HTMLButtonElement} nextButton
+ */
+function goToNextImage(scrollContainer, prevButton, nextButton) {
+  goToImage(scrollContainer, prevButton, nextButton, getGalleryItemIndex(scrollContainer) + 1)
+}
+
+/**
+ * @param {HTMLElement} scrollContainer
+ * @param {HTMLButtonElement} prevButton
+ * @param {HTMLButtonElement} nextButton
+ * @param {number} galleryItemIndex
+ */
+function goToImage(scrollContainer, prevButton, nextButton, galleryItemIndex) {
+  scrollContainer.scrollLeft = galleryItemIndex * scrollContainer.clientWidth
+  updateGalleryButtons(scrollContainer, prevButton, nextButton)
+}
+
+/**
+ * @param {HTMLElement} scrollContainer
+ * @param {HTMLButtonElement} prevButton
+ * @param {HTMLButtonElement} nextButton
+ */
+function updateGalleryButtons(scrollContainer, prevButton, nextButton) {
+  const galleryItemIndex = getGalleryItemIndex(scrollContainer)
+  prevButton.disabled = galleryItemIndex <= 0
+  nextButton.disabled = galleryItemIndex >= scrollContainer.children.length - 1
+}
+
+/**
+ * @param {HTMLElement} scrollContainer
+ * @returns {number}
  */
 function getGalleryItemIndex(scrollContainer) {
   return Math.round(scrollContainer.scrollLeft / scrollContainer.clientWidth)
