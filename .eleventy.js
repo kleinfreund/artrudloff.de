e30c4329348f38cfe208dedfbe4f68edeaa73fa2
kleinfreund <philipprudloff@fastmail.com>
2023-03-18 17:56:22 +0100

    Converts indentation to tabs

--- .eleventy.js
+++ .eleventy.js
@@ -6,57 +6,57 @@ const path = require('path')
 
 // https://github.com/kangax/html-minifier#options-quick-reference
 const htmlMinifierOptions = {
-  useShortDoctype: true,
-  removeComments: true,
-  collapseWhitespace: true
+	useShortDoctype: true,
+	removeComments: true,
+	collapseWhitespace: true
 }
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setLiquidOptions({
-    dynamicPartials: true,
-    strict_filters: true
-  })
+	eleventyConfig.setLiquidOptions({
+		dynamicPartials: true,
+		strict_filters: true
+	})
 
-  eleventyConfig.setDataDeepMerge(true)
+	eleventyConfig.setDataDeepMerge(true)
 
-  // Copies static files to the output directory
-  eleventyConfig
-    .addPassthroughCopy('src/img')
-    .addPassthroughCopy('src/css')
-    .addPassthroughCopy('src/js')
-    .addPassthroughCopy('src/favicon.ico')
-    .addPassthroughCopy('src/.htaccess')
+	// Copies static files to the output directory
+	eleventyConfig
+		.addPassthroughCopy('src/img')
+		.addPassthroughCopy('src/css')
+		.addPassthroughCopy('src/js')
+		.addPassthroughCopy('src/favicon.ico')
+		.addPassthroughCopy('src/.htaccess')
 
-  // Filter for compressing CSS/JS
-  eleventyConfig.addFilter('resolve_css_imports', resolveCssImports)
-  eleventyConfig.addFilter('minify_css', minifyCss)
-  eleventyConfig.addFilter('minify_js', minifyJs)
+	// Filter for compressing CSS/JS
+	eleventyConfig.addFilter('resolve_css_imports', resolveCssImports)
+	eleventyConfig.addFilter('minify_css', minifyCss)
+	eleventyConfig.addFilter('minify_js', minifyJs)
 
-  // Compresses output HTML
-  if (process.env.NODE_ENV === 'production') {
-    eleventyConfig.addTransform('minify_html', minifyHtml)
-  }
+	// Compresses output HTML
+	if (process.env.NODE_ENV === 'production') {
+		eleventyConfig.addTransform('minify_html', minifyHtml)
+	}
 
-  if (process.env.NODE_ENV !== 'production') {
-    // Log and print template data
-    eleventyConfig.addFilter('log', function (value) {
-      try {
-        console.log(JSON.stringify(value, null, 2))
-      } catch { }
+	if (process.env.NODE_ENV !== 'production') {
+		// Log and print template data
+		eleventyConfig.addFilter('log', function (value) {
+			try {
+				console.log(JSON.stringify(value, null, 2))
+			} catch { }
 
-      return require('util').inspect(value)
-    })
-  }
+			return require('util').inspect(value)
+		})
+	}
 
-  return {
-    dir: {
-      input: 'src',
-      // Make the project directory the includes directory. This allows me to include files from
-      // across the project instead of just a dedicated includes directory.
-      includes: ''
-    },
-    templateFormats: ['md', 'liquid', 'html']
-  }
+	return {
+		dir: {
+			input: 'src',
+			// Make the project directory the includes directory. This allows me to include files from
+			// across the project instead of just a dedicated includes directory.
+			includes: ''
+		},
+		templateFormats: ['md', 'liquid', 'html']
+	}
 }
 
 /**
@@ -64,12 +64,12 @@ module.exports = function (eleventyConfig) {
  * @returns {string} the concatenated contents of the CSS files found by resolving `@import` rules in the CSS file at `cssPath`.
  */
 function resolveCssImports(cssPath) {
-  return fs.readFileSync(path.resolve(__dirname, path.join('src', cssPath)), 'utf8')
-    .split(/\r?\n/)
-    .filter((line) => line.startsWith('@import'))
-    .map((rule) => rule.replace(/@import ['"]/, '').replace(/['"];/, ''))
-    .map((importPath) => fs.readFileSync(path.resolve(__dirname, path.join('src', importPath)), 'utf8'))
-    .join('')
+	return fs.readFileSync(path.resolve(__dirname, path.join('src', cssPath)), 'utf8')
+		.split(/\r?\n/)
+		.filter((line) => line.startsWith('@import'))
+		.map((rule) => rule.replace(/@import ['"]/, '').replace(/['"];/, ''))
+		.map((importPath) => fs.readFileSync(path.resolve(__dirname, path.join('src', importPath)), 'utf8'))
+		.join('')
 }
 
 /**
@@ -79,19 +79,19 @@ function resolveCssImports(cssPath) {
  * @returns {string} the minified CSS content
  */
 function minifyCss(concatenatedCssContent) {
-  const minifyResult = new CleanCSS().minify(concatenatedCssContent)
+	const minifyResult = new CleanCSS().minify(concatenatedCssContent)
 
-  if (minifyResult.errors.length > 0) {
-    console.error('❌ Could not minify CSS.')
+	if (minifyResult.errors.length > 0) {
+		console.error('❌ Could not minify CSS.')
 
-    for (const error of minifyResult.errors) {
-      console.error('❌', error)
-    }
+		for (const error of minifyResult.errors) {
+			console.error('❌', error)
+		}
 
-    return concatenatedCssContent
-  }
+		return concatenatedCssContent
+	}
 
-  return minifyResult.styles
+	return minifyResult.styles
 }
 
 /**
@@ -102,9 +102,9 @@ function minifyCss(concatenatedCssContent) {
  * @returns {string} the minified HTML content
  */
 function minifyHtml(content, outputPath) {
-  return outputPath.endsWith('.html')
-    ? htmlMinifier.minify(content, htmlMinifierOptions)
-    : content
+	return outputPath.endsWith('.html')
+		? htmlMinifier.minify(content, htmlMinifierOptions)
+		: content
 }
 
 /**
@@ -112,11 +112,11 @@ function minifyHtml(content, outputPath) {
  * @returns {Promise<string>}
  */
 async function minifyJs(content) {
-  try {
-    const result = await minify(content, { mangle: { toplevel: true } })
-    return result.code ?? ''
-  } catch (error) {
-    console.error('❌', error)
-    return ''
-  }
+	try {
+		const result = await minify(content, { mangle: { toplevel: true } })
+		return result.code ?? ''
+	} catch (error) {
+		console.error('❌', error)
+		return ''
+	}
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,179 +1,179 @@
 function initGallery() {
-  document.addEventListener('keydown', handleKeyboardShortcut)
-  window.customElements.define('image-gallery', ImageGallery)
+	document.addEventListener('keydown', handleKeyboardShortcut)
+	window.customElements.define('image-gallery', ImageGallery)
 }
 
 /**
  * @param {KeyboardEvent} event
  */
 function handleKeyboardShortcut(event) {
-  if (event.target instanceof Element) {
-    const component = /** @type {ImageGallery | null} */ (event.target.closest('.gallery'))
+	if (event.target instanceof Element) {
+		const component = /** @type {ImageGallery | null} */ (event.target.closest('.gallery'))
 
-    if (component !== null) {
-      component.triggerCommand(event)
-    }
-  }
+		if (component !== null) {
+			component.triggerCommand(event)
+		}
+	}
 }
 
 class ImageGallery extends HTMLElement {
-  /** @type {boolean} */ hasLoadedHighResImages = false
+	/** @type {boolean} */ hasLoadedHighResImages = false
 
-  /** @type {HTMLElement} */ scrollContainer
-  /** @type {HTMLButtonElement} */ openOverlayButton
-  /** @type {HTMLButtonElement} */ closeOverlayButton
-  /** @type {HTMLButtonElement | null} */ prevButton
-  /** @type {HTMLButtonElement | null} */ nextButton
+	/** @type {HTMLElement} */ scrollContainer
+	/** @type {HTMLButtonElement} */ openOverlayButton
+	/** @type {HTMLButtonElement} */ closeOverlayButton
+	/** @type {HTMLButtonElement | null} */ prevButton
+	/** @type {HTMLButtonElement | null} */ nextButton
 
-  /** @type {Record<string, (event: KeyboardEvent) => void>} */ commands = {
-    Escape: () => {
-      if (this.classList.contains('gallery--is-overlay')) {
-        this.closeOverlay()
-      }
-    },
+	/** @type {Record<string, (event: KeyboardEvent) => void>} */ commands = {
+		Escape: () => {
+			if (this.classList.contains('gallery--is-overlay')) {
+				this.closeOverlay()
+			}
+		},
 
-    Enter: () => {
-      if (!this.classList.contains('gallery--is-overlay') && this.scrollContainer === this.ownerDocument.activeElement) {
-        this.scrollContainer.dispatchEvent(new Event('click'))
-      }
-    },
-  }
+		Enter: () => {
+			if (!this.classList.contains('gallery--is-overlay') && this.scrollContainer === this.ownerDocument.activeElement) {
+				this.scrollContainer.dispatchEvent(new Event('click'))
+			}
+		},
+	}
 
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.scrollContainer = /** @type {HTMLElement} */ (this.querySelector('.gallery-scroll-container'))
-    this.openOverlayButton = /** @type {HTMLButtonElement} */ (this.querySelector('.gallery-open-overlay-button'))
-    this.closeOverlayButton = /** @type {HTMLButtonElement} */ (this.querySelector('.gallery-close-overlay-button'))
-    this.prevButton = /** @type {HTMLButtonElement | null} */ (this.querySelector('.gallery-controls__prev-button'))
-    this.nextButton = /** @type {HTMLButtonElement | null} */ (this.querySelector('.gallery-controls__next-button'))
+		this.scrollContainer = /** @type {HTMLElement} */ (this.querySelector('.gallery-scroll-container'))
+		this.openOverlayButton = /** @type {HTMLButtonElement} */ (this.querySelector('.gallery-open-overlay-button'))
+		this.closeOverlayButton = /** @type {HTMLButtonElement} */ (this.querySelector('.gallery-close-overlay-button'))
+		this.prevButton = /** @type {HTMLButtonElement | null} */ (this.querySelector('.gallery-controls__prev-button'))
+		this.nextButton = /** @type {HTMLButtonElement | null} */ (this.querySelector('.gallery-controls__next-button'))
 
-    this.updateGalleryButtonDisabledState()
-  }
+		this.updateGalleryButtonDisabledState()
+	}
 
-  get [Symbol.toStringTag]() {
-    return 'ImageGallery'
-  }
+	get [Symbol.toStringTag]() {
+		return 'ImageGallery'
+	}
 
-  connectedCallback() {
-    if (!this.isConnected) {
-      return
-    }
+	connectedCallback() {
+		if (!this.isConnected) {
+			return
+		}
 
-    const debouncedHandleScrollContainerScrollEvent = debounce(() => this.handleScrollContainerScroll, 50)
-    this.scrollContainer.addEventListener('scroll', debouncedHandleScrollContainerScrollEvent)
-    this.scrollContainer.addEventListener('click', this.handleScrollContainerClick)
-    this.openOverlayButton.addEventListener('click', this.handleOpenOverlayButtonClick)
-    this.closeOverlayButton.addEventListener('click', this.handleCloseOverlayButtonClick)
-    this.prevButton?.addEventListener('click', this.handlePrevButtonClick)
-    this.nextButton?.addEventListener('click', this.handleNextButtonClick)
-  }
+		const debouncedHandleScrollContainerScrollEvent = debounce(() => this.handleScrollContainerScroll, 50)
+		this.scrollContainer.addEventListener('scroll', debouncedHandleScrollContainerScrollEvent)
+		this.scrollContainer.addEventListener('click', this.handleScrollContainerClick)
+		this.openOverlayButton.addEventListener('click', this.handleOpenOverlayButtonClick)
+		this.closeOverlayButton.addEventListener('click', this.handleCloseOverlayButtonClick)
+		this.prevButton?.addEventListener('click', this.handlePrevButtonClick)
+		this.nextButton?.addEventListener('click', this.handleNextButtonClick)
+	}
 
-  handleScrollContainerScroll = () => {
-    this.updateGalleryButtonDisabledState()
-  }
+	handleScrollContainerScroll = () => {
+		this.updateGalleryButtonDisabledState()
+	}
 
-  handleScrollContainerClick = () => {
-    this.toggleOverlay()
-  }
+	handleScrollContainerClick = () => {
+		this.toggleOverlay()
+	}
 
-  handleOpenOverlayButtonClick = () => {
-    this.toggleOverlay()
-  }
+	handleOpenOverlayButtonClick = () => {
+		this.toggleOverlay()
+	}
 
-  handleCloseOverlayButtonClick = () => {
-    this.toggleOverlay()
-  }
+	handleCloseOverlayButtonClick = () => {
+		this.toggleOverlay()
+	}
 
-  handlePrevButtonClick = () => {
-    this.goToImage(this.getGalleryItemIndex() - 1)
-  }
+	handlePrevButtonClick = () => {
+		this.goToImage(this.getGalleryItemIndex() - 1)
+	}
 
-  handleNextButtonClick = () => {
-    this.goToImage(this.getGalleryItemIndex() + 1)
-  }
+	handleNextButtonClick = () => {
+		this.goToImage(this.getGalleryItemIndex() + 1)
+	}
 
-  toggleOverlay() {
-    // Retrieves the currently visible gallery item index **before** toggling the overlay mode which causes the scroll container to have different dimensions which would make that computation incorrect.
-    const galleryItemIndex = this.getGalleryItemIndex()
+	toggleOverlay() {
+		// Retrieves the currently visible gallery item index **before** toggling the overlay mode which causes the scroll container to have different dimensions which would make that computation incorrect.
+		const galleryItemIndex = this.getGalleryItemIndex()
 
-    if (this.classList.contains('gallery--is-overlay')) {
-      this.closeOverlay()
-    } else {
-      this.openOverlay()
-    }
+		if (this.classList.contains('gallery--is-overlay')) {
+			this.closeOverlay()
+		} else {
+			this.openOverlay()
+		}
 
-    // Corrects the scroll position of the currently visible gallery item as the scroll container generally has different dimensions in overlay mode. That's why the correct index is determined **before** switching modes.
-    setTimeout(() => {
-      this.goToImage(galleryItemIndex)
-    }, 0)
-  }
+		// Corrects the scroll position of the currently visible gallery item as the scroll container generally has different dimensions in overlay mode. That's why the correct index is determined **before** switching modes.
+		setTimeout(() => {
+			this.goToImage(galleryItemIndex)
+		}, 0)
+	}
 
-  openOverlay() {
-    this.classList.add('gallery--is-overlay')
-    this.ownerDocument.body.style.setProperty('overflow', 'hidden')
+	openOverlay() {
+		this.classList.add('gallery--is-overlay')
+		this.ownerDocument.body.style.setProperty('overflow', 'hidden')
 
-    if (!this.hasLoadedHighResImages) {
-      this.hasLoadedHighResImages = true
+		if (!this.hasLoadedHighResImages) {
+			this.hasLoadedHighResImages = true
 
-      for (const image of this.querySelectorAll('[data-high-res-src]')) {
-        image.setAttribute('src', /** @type {string} */(image.getAttribute('data-high-res-src')))
-        image.removeAttribute('data-high-res-src')
-      }
-    }
+			for (const image of this.querySelectorAll('[data-high-res-src]')) {
+				image.setAttribute('src', /** @type {string} */(image.getAttribute('data-high-res-src')))
+				image.removeAttribute('data-high-res-src')
+			}
+		}
 
-    // Ensures that triggering the open overlay button doesn't also trigger the close button.
-    setTimeout(() => {
-      this.closeOverlayButton.focus()
-    }, 0)
-  }
+		// Ensures that triggering the open overlay button doesn't also trigger the close button.
+		setTimeout(() => {
+			this.closeOverlayButton.focus()
+		}, 0)
+	}
 
-  closeOverlay() {
-    this.classList.remove('gallery--is-overlay')
-    this.ownerDocument.body.style.removeProperty('overflow')
+	closeOverlay() {
+		this.classList.remove('gallery--is-overlay')
+		this.ownerDocument.body.style.removeProperty('overflow')
 
-    // Ensures that triggering the close overlay button doesn't also trigger the scroll container's click event listener.
-    setTimeout(() => {
-      this.scrollContainer.focus()
-    }, 0)
-  }
+		// Ensures that triggering the close overlay button doesn't also trigger the scroll container's click event listener.
+		setTimeout(() => {
+			this.scrollContainer.focus()
+		}, 0)
+	}
 
-  /**
-   * @param {number} galleryItemIndex
-   */
-  goToImage(galleryItemIndex) {
-    this.scrollContainer.scrollLeft = galleryItemIndex * this.scrollContainer.clientWidth
-    this.updateGalleryButtonDisabledState()
-  }
+	/**
+	 * @param {number} galleryItemIndex
+	 */
+	goToImage(galleryItemIndex) {
+		this.scrollContainer.scrollLeft = galleryItemIndex * this.scrollContainer.clientWidth
+		this.updateGalleryButtonDisabledState()
+	}
 
-  updateGalleryButtonDisabledState() {
-    if (!(this.prevButton instanceof HTMLButtonElement && this.nextButton instanceof HTMLButtonElement)) {
-      return
-    }
+	updateGalleryButtonDisabledState() {
+		if (!(this.prevButton instanceof HTMLButtonElement && this.nextButton instanceof HTMLButtonElement)) {
+			return
+		}
 
-    const galleryItemIndex = this.getGalleryItemIndex()
+		const galleryItemIndex = this.getGalleryItemIndex()
 
-    this.prevButton.disabled = galleryItemIndex <= 0
-    this.nextButton.disabled = galleryItemIndex >= this.scrollContainer.children.length - 1
-  }
+		this.prevButton.disabled = galleryItemIndex <= 0
+		this.nextButton.disabled = galleryItemIndex >= this.scrollContainer.children.length - 1
+	}
 
-  /**
-   * @returns {number}
-   */
-  getGalleryItemIndex() {
-    return Math.round(this.scrollContainer.scrollLeft / this.scrollContainer.clientWidth)
-  }
+	/**
+	 * @returns {number}
+	 */
+	getGalleryItemIndex() {
+		return Math.round(this.scrollContainer.scrollLeft / this.scrollContainer.clientWidth)
+	}
 
-  /**
-   * @param {KeyboardEvent} event
-   */
-  triggerCommand(event) {
-    const command = this.commands[event.code]
+	/**
+	 * @param {KeyboardEvent} event
+	 */
+	triggerCommand(event) {
+		const command = this.commands[event.code]
 
-    if (command) {
-      command(event)
-    }
-  }
+		if (command) {
+			command(event)
+		}
+	}
 }
 
 /**
@@ -186,20 +186,20 @@ class ImageGallery extends HTMLElement {
  * @returns {(...args: any) => void} the debounced function.
  */
 function debounce(initialFunction, delay) {
-  // Store timeout ID outside the returned function.
-  /** @type {number} */ let timeoutId
+	// Store timeout ID outside the returned function.
+	/** @type {number} */ let timeoutId
 
-  return (/** @type {any[]} */ ...args) => {
-    // If the debounced function was already invoked before, this will cancel
-    // the earlier timeout; thus, its callback will not be invoked.
-    clearTimeout(timeoutId)
+	return (/** @type {any[]} */ ...args) => {
+		// If the debounced function was already invoked before, this will cancel
+		// the earlier timeout; thus, its callback will not be invoked.
+		clearTimeout(timeoutId)
 
-    // Starts a new timer which will call the initial function after the
-    // specified wait time unless the debounced function is called again.
-    timeoutId = window.setTimeout(() => {
-      initialFunction(...args)
-    }, delay)
-  }
+		// Starts a new timer which will call the initial function after the
+		// specified wait time unless the debounced function is called again.
+		timeoutId = window.setTimeout(() => {
+			initialFunction(...args)
+		}, delay)
+	}
 }
 
 initGallery()
